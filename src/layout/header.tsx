2a9726ba3e269f5d457e4778f57c13b95fffbc2d
HuanTian <HuanTian3365543554@outlook.com>
2026-05-07 20:45:35 +0800

    style: 添加图标到头部按钮，增强用户界面交互性

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,5 +1,6 @@
 import { Button, Layout } from "antd";
 import style from "./header.module.css";
+import { GithubOutlined, SunOutlined } from "@ant-design/icons";
 
 const { Header: AntdHeader } = Layout;
 
@@ -9,8 +10,8 @@ export default function Header() {
       <div className={style.content}>
         <div>HTML Explorer</div>
         <div className={style.actions}>
-          <Button type="text" size="large" />
-          <Button type="text" size="large" />
+          <Button type="text" size="large" icon={<SunOutlined />} />
+          <Button type="text" size="large" icon={<GithubOutlined />} />
         </div>
       </div>
     </AntdHeader>
